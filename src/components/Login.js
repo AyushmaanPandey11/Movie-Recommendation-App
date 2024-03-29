a6f0510a7316134d679ae2004a017d36b8c612de
Ayushmaan Pandey <ayushmaan1122pandey@gmail.com>
2024-03-29 16:11:41 +0530

    Sign in - Sign up form toggle

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import {BG_URL} from '../utils/constants';
 const Login = () => {
+  const [isSignIn,setIsSignIn] = useState(true);
+  const ToggleSignInForm = () => {
+    setIsSignIn(!isSignIn);
+  };
   return (
     <div className='bg-black'>
       <Header />
@@ -10,11 +14,12 @@ const Login = () => {
             alt="Bg_img" className='max-w-full'/>
       </div>
       <form className='w-3/12 absolute p-12 my-40 text-white bg-black mx-auto left-0 right-0 bg-opacity-75 rounded-md'  >
-          <h2 className='font-bold text-3xl py-4'>Sign in</h2>
-          <input className='my-6 p-4 w-full bg-gray-600 ' type='text' placeholder='Email Address'></input>
-          <input className='my-6 p-4 w-full bg-gray-600 ' type='password' placeholder='Password'></input>
-          <button className='my-6 p-4 w-full bg-red-800'>Sign in</button>
-          <p className='my-2 p-2'  >New to Netlfix? Sign Up now!</p>
+          <h2 className='font-bold text-3xl py-4'>{isSignIn ? "Sign In" : "Sign Up"}</h2>
+          { !isSignIn && <input className='my-6 p-4 w-full bg-gray-600 ' type='text' placeholder='Full Name'></input>}
+          <input className='my-4 p-4 w-full bg-gray-600 ' type='text' placeholder='Email Address'></input>
+          <input className='my-4 p-4 w-full bg-gray-600 ' type='password' placeholder='Password'></input>
+          <button className='my-4 p-4 w-full bg-red-800'>{isSignIn ? "Sign In" : "Sign Up"}</button>
+          <p className='my-2 p-2 cursor-pointer' onClick={ToggleSignInForm} >{isSignIn ? "New to Netlfix? Sign Up now!" : "Already a Member? Sign in now"}</p>
         </form>
     </div>
   )
