ddcb3609bc11cf0123b8f34e0ce8e5bd3659834c
Ayushmaan Pandey <ayushmaan1122pandey@gmail.com>
2024-04-03 21:51:02 +0530

    Hooks Bug fixed

--- src/Hooks/useNowPlayingMovies.js
+++ src/Hooks/useNowPlayingMovies.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import  { useCallback, useEffect } from 'react'
 import { useDispatch } from 'react-redux';
 import { API_OPTIONS } from '../utils/constants';
 import { addNowPlayingMovies } from '../utils/movieSlice';
@@ -6,16 +6,16 @@ import { addNowPlayingMovies } from '../utils/movieSlice';
 const useNowPlayingMovies = async () => {
     const dispatch = useDispatch();
 
-    const getNowPlayingMovies = async () => {
+    const getNowPlayingMovies = useCallback(async () => {
       const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS);
       const json = await data.json();
       dispatch(addNowPlayingMovies(json.results));  
-    };
+    },[dispatch]);
   
     useEffect( () => {
       getNowPlayingMovies();
     }
-    ,[]);
+    ,[dispatch,getNowPlayingMovies]);
 }
 
 export default useNowPlayingMovies;
--- src/Hooks/useVideoTrialer.js
+++ src/Hooks/useVideoTrialer.js
@@ -1,21 +1,26 @@
-import { useEffect } from 'react'
+import { useEffect, useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import { addVideoTrialer } from '../utils/movieSlice';
 import { API_OPTIONS } from '../utils/constants';
 
-const useVideoTrialer =  ({movie_id}) => {
+const useVideoTrialer = ({ movie_id }) => {
     const dispatch = useDispatch();
-    const getMovieVideo = async () => 
-    {
-        const data =await fetch("https://api.themoviedb.org/3/movie/"+movie_id+"/videos?language=en-US", API_OPTIONS);
-        const json = await data.json();
-        const filterData = json.results.filter((video) => video.type === "Trailer" );
-        const trialer = filterData.length ? filterData[0] : json.results[0];
-        dispatch(addVideoTrialer(trialer));
-    };
-  useEffect(()=> {
-    getMovieVideo();
-  },[movie_id, dispatch]);
-}
+
+    const getMovieVideo = useCallback(async () => {
+        try {
+            const data = await fetch(`https://api.themoviedb.org/3/movie/${movie_id}/videos?language=en-US`, API_OPTIONS);
+            const json = await data.json();
+            const filterData = json.results.filter((video) => video.type === "Trailer");
+            const trialer = filterData.length ? filterData[0] : json.results[0];
+            dispatch(addVideoTrialer(trialer));
+        } catch (error) {
+            console.error("Error fetching movie video:", error);
+        }
+    }, [dispatch, movie_id]);
+
+    useEffect(() => {
+        getMovieVideo();
+    }, [getMovieVideo, dispatch]);
+};
 
 export default useVideoTrialer;
