934a39470f267601e1ed5b874ec2f53f82336051
Ayushmaan Pandey <ayushmaan1122pandey@gmail.com>
2024-04-11 05:47:36 +0530

    User input for api key open ai

--- src/utils/languageConstants.js
+++ src/utils/languageConstants.js
@@ -3,11 +3,13 @@ const lang = {
         search: "Search",
         gptSearchPlaceholderValue: "What would you like to watch today?", 
         gptSearchBtn: "GPT Search",
+        apiKey : "Give Your Api Key"
     },
     hindi: {
         search: "खोज",
         gptSearchPlaceholderValue: "आप आज क्या देखना चाहेंगे?",
         gptSearchBtn:"जीपीटी खोज",
+        apiKey : "अपनी एपीआई कुंजी दें",
     },
 
 };
--- src/utils/userSlice.js
+++ src/utils/userSlice.js
@@ -3,19 +3,24 @@ import { createSlice } from "@reduxjs/toolkit";
 const userSlice = createSlice(
     {
         name : "user",
-        initialState: null,
+        initialState: {
+            openAiKey: null,
+        },
         reducers : {
             addUser : (state,action) => {
                 return action.payload;
             },
             removeUser : (state,action) => {
                 return null;
+            },   
+            addOpenAiKey: (state,action) => {
+                state.openAiKey = action.payload;
             }
 
         }
     }
 );
 
-export const { addUser, removeUser } = userSlice.actions;
+export const { addUser, removeUser, addOpenAiKey } = userSlice.actions;
 
 export default userSlice.reducer;
