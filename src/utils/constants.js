002f64a249f26b466e19761e8a2acc21e8680d97
Ayushmaan Pandey <ayushmaan1122pandey@gmail.com>
2024-05-11 16:41:57 +0530

    -

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,8 +5,7 @@ export const API_OPTIONS ={
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMTUzNGE3MzE2NjgyYWU5OWRmOTljZTkyNjBjZDU2YSIsInN1YiI6IjY2MGJjNjJjY2FiZmU0MDE3ZTMwM2NkZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.l0rtRvZjmI2zaDFC9gb6JSQ0cVB7oU-zVGmjts869As" 
-    //+ process.env.REACT_APP_TMDB_KEY ,
+    Authorization: "Bearer " + process.env.REACT_APP_TMDB_KEY ,
   }
 };
 
